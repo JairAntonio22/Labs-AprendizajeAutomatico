142f80333c9278e65065d408987ee6eff164fe1d
MaxZambada <max.zambada@hotmail.com>
2021-09-22 13:49:02 -0500

    Aplicacion de Arboles con Iris, e intento con los otros dos pero falla

--- Prácticas/Práctica_5/practice5.py
+++ Prácticas/Práctica_5/practice5.py
@@ -30,43 +30,6 @@ def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
         plt.tight_layout()
     plt.savefig(path, format=fig_extension, dpi=resolution)
 
-#######################################
-#Inducir arbol de decision usando dataset de:
-# https://scikit-learn.org/stable/auto_examples/datasets/plot_iris_dataset.html
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-
-tree_clf = tree.DecisionTreeClassifier(max_depth=3)
-tree_clf.fit(X_train, y_train)
-
-
-#######################################
-
-#Dibujar modelo del árbol de decisión
-export_graphviz(
-        tree_clf,
-        out_file=os.path.join(IMAGES_PATH, "iris_tree.dot"),
-        feature_names=iris.feature_names[2:],
-        class_names=iris.target_names,
-        rounded=True,
-        filled=True
-    )
-
-Source.from_file(os.path.join(IMAGES_PATH, "iris_tree.dot"))
-
-#Visualizar modelo al instalar el paquete graphviz,
-#y convertir archivo DOT en PNG con comando:
-# dot -Tpng iris_tree.dot -o iris_tree.png
-
-#Probar árbol de decisión final con conjuntos de Train y Test
-
-#Calcular precisión del modelo
-print(tree_clf.score(X_test, y_test))
-
-#Visualizar particiones generadas por cada split
 def plot_decision_boundary(clf, X, y, axes=[0, 7.5, 0, 3], iris=True, legend=False,
 plot_training=True):
     x1s = np.linspace(axes[0], axes[1], 100)
@@ -92,16 +55,133 @@ plot_training=True):
         plt.ylabel(r"$x_2$", fontsize=18, rotation=0)
     if legend:
         plt.legend(loc="lower right", fontsize=14)
+#######################################
+#Inducir arbol de decision usando dataset de:
+# https://scikit-learn.org/stable/auto_examples/datasets/plot_iris_dataset.html
+def iris_procedure():
+    iris = datasets.load_iris()
+    X = iris.data[:,:2]
+    y = iris.target
 
-plt.figure(figsize=(8, 4))
-plot_decision_boundary(tree_clf, X, y)
-plt.plot([2.45, 2.45], [0, 3], "k-", linewidth=2)
-plt.plot([2.45, 7.5], [1.75, 1.75], "k--", linewidth=2)
-plt.text(1.40, 1.0, "Depth=0", fontsize=15)
-plt.text(3.2, 1.80, "Depth=1", fontsize=13)
-save_fig("decision_tree_decision_boundaries_plot")
-plt.show()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+    tree_clf = tree.DecisionTreeClassifier(max_depth=3)
+    tree_clf.fit(X_train, y_train)
+
+    #Dibujar modelo del árbol de decisión
+    export_graphviz(
+            tree_clf,
+            out_file=os.path.join(IMAGES_PATH, "iris_tree.dot"),
+            feature_names=iris.feature_names[2:],
+            class_names=iris.target_names,
+            rounded=True,
+            filled=True
+        )
+
+    Source.from_file(os.path.join(IMAGES_PATH, "iris_tree.dot"))
+
+    #Visualizar modelo al instalar el paquete graphviz,
+    #y convertir archivo DOT en PNG con comando:
+    # dot -Tpng iris_tree.dot -o iris_tree.png
+    #Probar árbol de decisión final con conjuntos de Train y Test
+    #Calcular precisión del modelo
+    precision = tree_clf.score(X_test, y_test) * 100
+    print("IRIS DECISION TREE PRECISION:")
+    print(precision)
+
+    plt.figure(figsize=(8, 4))
+    plot_decision_boundary(tree_clf, X, y)
+    plt.plot([2.45, 2.45], [0, 3], "k-", linewidth=2)
+    plt.plot([2.45, 7.5], [1.75, 1.75], "k--", linewidth=2)
+    plt.text(1.40, 1.0, "Depth=0", fontsize=15)
+    plt.text(3.2, 1.80, "Depth=1", fontsize=13)
+    save_fig("iris_decision_tree_decision_boundaries_plot")
+    plt.show()
+
+def wine_procedure():
+    wine = datasets.load_wine()
+    X = wine.data[:2,:2]
+    y = wine.target
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+    tree_clf = tree.DecisionTreeClassifier(max_depth=3)
+    tree_clf.fit(X_train, y_train)
+
+    #Dibujar modelo del árbol de decisión
+    export_graphviz(
+            tree_clf,
+            out_file=os.path.join(IMAGES_PATH, "wine_tree.dot"),
+            feature_names=wine.feature_names[2:],
+            class_names=wine.target_names,
+            rounded=True,
+            filled=True
+        )
+
+    Source.from_file(os.path.join(IMAGES_PATH, "wine_tree.dot"))
+
+    #Visualizar modelo al instalar el paquete graphviz,
+    #y convertir archivo DOT en PNG con comando:
+    # dot -Tpng iris_tree.dot -o iris_tree.png
+    #Probar árbol de decisión final con conjuntos de Train y Test
+    #Calcular precisión del modelo
+    precision = tree_clf.score(X_test, y_test) * 100
+    print("WINE DECISION TREE PRECISION:")
+    print(precision)
+
+    plt.figure(figsize=(8, 4))
+    plot_decision_boundary(tree_clf, X, y)
+    plt.plot([2.45, 2.45], [0, 3], "k-", linewidth=2)
+    plt.plot([2.45, 7.5], [1.75, 1.75], "k--", linewidth=2)
+    plt.text(1.40, 1.0, "Depth=0", fontsize=15)
+    plt.text(3.2, 1.80, "Depth=1", fontsize=13)
+    save_fig("wine_decision_tree_decision_boundaries_plot")
+    plt.show()
+
+def breast_procedure():
+    breast = datasets.load_breast_cancer()
+    X = breast.data[:,:2]
+    y = breast.target
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+    tree_clf = tree.DecisionTreeClassifier(max_depth=3)
+    tree_clf.fit(X_train, y_train)
+
+    #Dibujar modelo del árbol de decisión
+    export_graphviz(
+            tree_clf,
+            out_file=os.path.join(IMAGES_PATH, "breast_cancer_tree.dot"),
+            feature_names=wine.feature_names[2:],
+            class_names=wine.target_names,
+            rounded=True,
+            filled=True
+        )
+
+    Source.from_file(os.path.join(IMAGES_PATH, "breast_cancer_tree.dot"))
+
+    #Visualizar modelo al instalar el paquete graphviz,
+    #y convertir archivo DOT en PNG con comando:
+    # dot -Tpng iris_tree.dot -o iris_tree.png
+    #Probar árbol de decisión final con conjuntos de Train y Test
+    #Calcular precisión del modelo
+    precision = tree_clf.score(X_test, y_test) * 100
+    print("BREAST CANCER DECISION TREE PRECISION:")
+    print(precision)
+
+    plt.figure(figsize=(8, 4))
+    plot_decision_boundary(tree_clf, X, y)
+    plt.plot([2.45, 2.45], [0, 3], "k-", linewidth=2)
+    plt.plot([2.45, 7.5], [1.75, 1.75], "k--", linewidth=2)
+    plt.text(1.40, 1.0, "Depth=0", fontsize=15)
+    plt.text(3.2, 1.80, "Depth=1", fontsize=13)
+    save_fig("breast_cancer_decision_tree_decision_boundaries_plot")
+    plt.show()
 
 ##Hacer lo mismo para wine y breast_cancer
 #wine: https://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_wine.html
 #breast_cancer: https://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_breast_cancer.html
+
+iris_procedure()
+wine_procedure()
+breast_procedure()
