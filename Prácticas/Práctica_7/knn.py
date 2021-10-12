f9e590c45dbbc5b30ea4e6dffb47af721dcfe017
Jair Antonio <jair.loranca@gmail.com>
2021-10-11 19:03:08 -0500

    Practica 7 ajustes

--- Prácticas/Práctica_7/knn.py
+++ Prácticas/Práctica_7/knn.py
@@ -4,21 +4,27 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.preprocessing import label_binarize
 
 
 def main():
     ''' Main script '''
-    X, y = load_digits(return_X_y=True)
+    digits = load_digits()
+    X = digits.data
+    y = digits.target
+
+    y = label_binarize(y, classes=[i for i in range(10)])
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=.80, random_state=42
     )
 
-    neighbors = [k for k in range(1, 350)]
+    neighbors = [k for k in range(1, 359)]
     scores = []
 
-    for k in range(1, 350):
-        clf = KNeighborsClassifier(n_neighbors=k)
+    for k in range(1, 359):
+        clf = OneVsRestClassifier(KNeighborsClassifier(n_neighbors=k))
         clf.fit(X_train, y_train)
         scores.append(clf.score(X_test, y_test) * 100)
 
